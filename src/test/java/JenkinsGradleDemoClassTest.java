4fbbe8f9d79fc741cb7c7a2756b3df74339447e2
fairy17 <1020645987@qq.com>
2018-12-14 11:03:00 +0800

    On class

--- src/test/java/JenkinsGradleDemoClassTest.java
+++ src/test/java/JenkinsGradleDemoClassTest.java
@@ -1,5 +1,3 @@
-package main.java;
-
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,7 +5,7 @@ import static org.junit.Assert.*;
 public class JenkinsGradleDemoClassTest {
     @Test
     public void op(){
-        JenkinsGradleDemoClass jenkinsGradleDemoClass=new JenkinsGradleDemoClass();
+        main.java.JenkinsGradleDemoClass jenkinsGradleDemoClass=new main.java.JenkinsGradleDemoClass();
         assertEquals(jenkinsGradleDemoClass.add(1,2),3);
     }
 }
